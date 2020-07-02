05b1df706a47b45f42be877d061ea0f7692dd336
Messier81 <nesanyorku@gmail.com>
2020-07-01 23:53:29 -0400

    added first proj RIV

--- src/comps/MainView.jsx
+++ src/comps/MainView.jsx
@@ -6,7 +6,6 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
 
 export default class MainView extends Component {
   render() {
@@ -19,26 +18,6 @@ export default class MainView extends Component {
           <Row className="justify-content-center">
             <Col xs="auto" className="myDesc">
               Developer &middot; Musician &middot; Writer
-            </Col>
-          </Row>
-          <Row className="justify-content-center">
-            <Col xs="auto">
-              <Button
-                href="https://github.com/Messier81"
-                variant="light"
-                className="socBut"
-              >
-                GITHUB
-              </Button>
-            </Col>
-            <Col xs="auto">
-              <Button
-                href="https://www.linkedin.com/in/sivanesan-vettivel-992318182/"
-                variant="light"
-                className="socBut"
-              >
-                LINKEDIN
-              </Button>
             </Col>
           </Row>
         </Container>
